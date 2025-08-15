2af5b247b13ba13372f6b425d8fa3eae981fc5d0
christine <christinetrant@gmail.com>
2025-08-15 13:44:28 +0100

    Remove console logs and unneeded  param

--- src/container/App.js
+++ src/container/App.js
@@ -5,35 +5,33 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { setSearchField } from "../store/actions";
 
-const App = ({store}) => {
-    console.log("🚀 ~ App ~ store:", store);
-    const dispatch = useDispatch();
-    const [monsters, setMonsters] = useState([]);
-    // const [searchString, setSearchString] = useState("");
-    const searchString = useSelector(state => state.searchRobots.searchField);
-    console.log("🚀 ~ App ~ searchString:", searchString);
+const App = () => {
+	const dispatch = useDispatch();
+	const [monsters, setMonsters] = useState([]);
+	// const [searchString, setSearchString] = useState("");
+	const searchString = useSelector((state) => state.searchRobots.searchField);
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((users) =>setMonsters(users));
-    }, []);
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((response) => response.json())
+			.then((users) => setMonsters(users));
+	}, []);
 
-    const onSearchChange = (e) => {
-        // setSearchString(e.target.value.toLowerCase());
-        dispatch(setSearchField(e.target.value.toLowerCase()));
-    };
+	const onSearchChange = (e) => {
+		// setSearchString(e.target.value.toLowerCase());
+		dispatch(setSearchField(e.target.value.toLowerCase()));
+	};
 
-    const filteredMonsters = monsters.filter((monster) => {
-        return monster.name.toLowerCase().includes(searchString);
-    });
-    return (
-        <div className="App">
-            <h1 className="app-title">Monsters!</h1>
-            <SearchBox onSearchChange={onSearchChange} placeholder="Search monsters..." />
-            <CardList items={filteredMonsters} />
-        </div>
-    );
+	const filteredMonsters = monsters.filter((monster) => {
+		return monster.name.toLowerCase().includes(searchString);
+	});
+	return (
+		<div className="App">
+			<h1 className="app-title">Monsters!</h1>
+			<SearchBox onSearchChange={onSearchChange} placeholder="Search monsters..." />
+			<CardList items={filteredMonsters} />
+		</div>
+	);
 };
 
 export default App;
