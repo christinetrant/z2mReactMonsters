b5dc5d782c96a627313e88d5bf44df23b0b6cf13
Christine <christinetrant@gmail.com>
2025-09-26 11:15:10 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
 	plugins: [react()],
+	base: "/z2mReactMonsters/", // 👈 must be top-level, not inside build
 	build: {
-		base: "/z2mReactMonsters/",
-		// outDir: "build", // CRA's default build output
+		// outDir: "build", // optional, defaults to "dist"
 	},
 });
