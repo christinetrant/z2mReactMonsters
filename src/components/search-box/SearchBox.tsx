c0cc997e9f22e81ac54efb49c0f3c9f00c3c912a
Christine <christinetrant@gmail.com>
2025-09-26 11:11:15 +0100

    Remove unneeded comments

--- src/components/search-box/SearchBox.tsx
+++ src/components/search-box/SearchBox.tsx
@@ -1,25 +1,14 @@
-import React, { ChangeEvent, ChangeEventHandler } from 'react'
-import './search-box.style.css'
-
-// interface ISearchBoxProps extends IChangeHandlerProps {
-// 	className: string;
-// 	placeholder?: string // e.g. string || null
-// }
-
-// interface IChangeHandlerProps {
-// 	onChangeHandler: (a: string) => void
-// }
+import React, { ChangeEvent, ChangeEventHandler } from "react";
+import "./search-box.style.css";
 
 type SearchBoxProps = {
 	className: string;
 	placeholder?: string; // e.g. string || null
-	// onChangeHandler: (a: string) => void
-	// onChangeHandler: ChangeEventHandler<HTMLInputElement>; // This will return void
 	onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
-}
+};
 
-const SearchBox = ({className, onChangeHandler, placeholder}: SearchBoxProps) => {
-		return <input type="search" className={`search-box ${className}`} placeholder={placeholder} onChange={onChangeHandler} />;
-}
+const SearchBox = ({ className, onChangeHandler, placeholder }: SearchBoxProps) => {
+	return <input type="search" className={`search-box ${className}`} placeholder={placeholder} onChange={onChangeHandler} />;
+};
 
-export default SearchBox
+export default SearchBox;
